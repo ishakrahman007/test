b87c316e34a51be549be3237fd331bcd2498ae9c
ishakrahman007 <write2greenrahman@gmail.com>
2023-05-05 15:09:09 -0400

    first from intellij

---
+++ src/test/java/rahman/global/CodeFromFazia.java
@@ -0,0 +1,72 @@
+package rahman.global;
+
+import junit.framework.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class CodeFromFazia {
+    public static void main(String[] args) throws InterruptedException {
+        ChromeDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        System.out.println("Browser maximize success");
+        driver.get("https://rahulshettyacademy.com/locatorspractice/");
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        String expectedUrl = "https://rahulshettyacademy.com/locatorspractice/";
+        String actualUrl = driver.getCurrentUrl();
+
+        Assert.assertEquals(expectedUrl, actualUrl);
+        System.out.println("True");
+
+        //id
+        driver.findElement(By.id("inputUsername")).sendKeys("Fazia");
+        System.out.println("User name is entered in the username field");
+        //name
+        driver.findElement(By.name("inputPassword")).sendKeys("12345");
+        System.out.println("Password  is entered in the Password field");
+        //cssSelector
+        driver.findElement(By.className("signInBtn")).click();
+        System.out.println("Click on button success");
+
+        System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
+        System.out.println("error displayed");
+
+        driver.findElement(By.linkText("Forgot your password?")).click();
+        System.out.println("Click on button success");
+
+        driver.findElement(By.cssSelector("input[placeholder='Name']")).sendKeys("Rahul");
+        System.out.println("User name entered with success");
+
+        driver.findElement(By.xpath("//input[@placeholder='Email']")).sendKeys("Rahul@gmail.com");
+        System.out.println("Password entered with success");
+
+        driver.findElement(By.xpath("//input[@type='text'][3]")).sendKeys("1234567");
+        System.out.println("Phone number entered with success");
+
+
+        driver.findElement(By.xpath("//button[contains(@class,'reset-pwd-btn')]")).click();
+        System.out.println("Click on btn success");
+
+        System.out.println(driver.findElement(By.cssSelector("p.infoMsg")).getText());
+
+        driver.findElement(By.cssSelector(".go-to-login-btn")).click();
+        System.out.println("Click on btn success");
+
+        driver.findElement(By.id("inputUsername")).sendKeys("Fazia");
+        System.out.println("User name is entered in the username field");
+        //name
+        driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");
+        System.out.println("Password is entered in the password field");
+
+        Thread.sleep(2000);
+        driver.findElement(By.cssSelector(".submit.signInBtn")).click();
+        System.out.println("This cssSelector does not work with click()");
+
+
+        Thread.sleep(3000);
+        driver.close();
+    }
+}
+
---
+++ src/test/java/rahman/global/Test.java
@@ -0,0 +1,50 @@
+package rahman.global;
+
+import junit.framework.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class Test {
+
+    public static void main(String[] args) throws InterruptedException {
+        ChromeDriver driver = new ChromeDriver();
+        System.out.println("Browser launch success");
+        driver.manage().window().maximize();
+        System.out.println("Browser maximized success");
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+
+       String currentURL =  driver.getCurrentUrl();
+        System.out.println("Current URL is "+currentURL);
+        String expectedURL = "https://rahulshettyacademy.com/AutomationPractice/";
+
+        Assert.assertEquals(currentURL,expectedURL);
+        System.out.println("Land is success");
+
+        driver.findElement(By.id("checkBoxOption1")).click();
+        System.out.println("On checkBoxOption1 click is success");
+
+        driver.findElement(By.id("name")).sendKeys("Abu");
+        System.out.println("Send Abu as a key success");
+
+        List<WebElement> radioButtons = driver.findElements(By.name("radioButton"));
+        for (WebElement ra:radioButtons){
+            if(ra.getAttribute("value").equals("radio2")){
+                ra.click();
+            }
+        }
+        System.out.println("Click on radio button 2 is success");
+
+
+
+
+
+
+        Thread.sleep(2000);
+        driver.close();
+
+    }
+
+}
